9433b6a7ea082d4fb6e6ee57c5619c5970ef87e0
iamAbayomi <oladinifolawiyo@gmail.com>
2023-02-08 21:44:10 +0100

    fix: add image - made home page responsive

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,14 +1,11 @@
 import { options } from "@/utils/dummyvalues";
-import { animatePortfolioItem } from "@/utils/helpers";
 import { IProjectsData } from "@/utils/types";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 const Index = () => {
   const router = useRouter();
-  const projectDescriptionRef = useRef<HTMLDivElement>(null);
-  const [projectSection, setProjectSection] = useState<boolean>(false);
 
   const { ref } = useInView(options);
   const { ref: secondRef } = useInView(options);
@@ -16,27 +13,33 @@ const Index = () => {
 
   let projectsData: IProjectsData[] = [
     {
+      id: 1,
       ref: ref,
-      src: "./design-system-main.svg",
+      src: "./design-system",
       title: "Design System",
       description: "A design system to help you build your react components.",
-      showDescription: false
+      showDescription: false,
+      linkToSite: ""
     },
     {
+      id: 2,
       ref: secondRef,
-      src: "./open-dao-main.jpg",
+      src: "./open-dao",
       title: " Open DAO Challenge",
       description: `Join the OpenDAO Community to discover, invest in, and vote on
     exceptional DAOs. You have the power!`,
-      showDescription: false
+      showDescription: false,
+      linkToSite: "https://dao-open-challenge.vercel.app/dao"
     },
     {
+      id: 3,
       ref: thirdRef,
-      src: "./spiinge-main.jpg",
+      src: "./spiinge",
       title: "Spiinge",
       description:
         "A product management application to build projects and track the lean development method.",
-      showDescription: false
+      showDescription: false,
+      linkToSite: ""
     }
   ];
 
@@ -44,14 +47,34 @@ const Index = () => {
     useState<IProjectsData[]>(projectsData);
   const projectSectionRef = useRef<HTMLDivElement>(null);
 
-  function onMouseOver() {
-    setProjectSection(true);
-    console.log("on mouse over");
-  }
-
   function viewHomePage() {
     router.push("/");
   }
+
+  function viewProjects(item: IProjectsData) {
+    router.push(`${item.linkToSite}`);
+  }
+
+  function nextPortfolio() {
+    const tempProjectsData = projectsDataState;
+    let temp = tempProjectsData[0];
+    let temp2 = tempProjectsData[2];
+    tempProjectsData[0] = tempProjectsData[1];
+    tempProjectsData[2] = temp;
+    tempProjectsData[1] = temp2;
+    setProjectsDataState(JSON.parse(JSON.stringify(tempProjectsData)));
+  }
+
+  function previousPoftfolio() {
+    const tempProjectsData = projectsDataState;
+    let temp = tempProjectsData[0];
+    let temp2 = tempProjectsData[2];
+    tempProjectsData[0] = temp2; //tempProjectsData[2];
+    tempProjectsData[2] = tempProjectsData[1];
+    tempProjectsData[1] = temp; //tempProjectsData[0];
+    setProjectsDataState(JSON.parse(JSON.stringify(tempProjectsData)));
+  }
+
   return (
     <div className="max-w-[1600px]">
       <div className="bg-black text-white min-h-screen pt-4 px-0 pb-[50px]">
@@ -60,36 +83,74 @@ const Index = () => {
             {" "}
             O.A
           </p>
-          <div className="flex ml-[71px] gap-6">
+          <div className="flex ml-[71px] gap-6 pointer">
             <p>Resume</p>
             <p>Blog</p>
           </div>
         </div>
         <div
-          className="projects-section flex  items-center
-             gap-[120px] mt-[50px] "
+          className="projects-section flex
+             justify-between gap-[0px] mt-[50px] "
           ref={projectSectionRef}
         >
           {projectsDataState?.map((item: IProjectsData, index) => (
-            <div className="mt-[10px] image-container">
-              <img
-                ref={item?.ref}
-                className="image-item"
-                src={item?.src}
-                onMouseOver={onMouseOver}
-              />
+            <div className="flex mt-[40px] items-center">
+              {index == 1 && (
+                <img
+                  src="./backward-arrow.svg"
+                  className="arrow mr-[25px] pointer"
+                  onClick={() =>
+                    setTimeout(() => {
+                      previousPoftfolio();
+                    }, 500)
+                  }
+                />
+              )}
               <div
-                className={`projects-description mt-[70px] mx-auto w-[550px] text-center`}
+                className={`image-container pointer ${
+                  index == 1 && "center-image-container"
+                }`}
               >
-                <p className="text-center text-[24px] font-bold">
-                  {item?.title}
-                </p>
-                <p className=" mt-[18px] font-normal">{item?.description}</p>
+                <img
+                  ref={item?.ref}
+                  className={`image-item pointer ${
+                    index == 1 ? "center-image-item" : "side-image"
+                  }`}
+                  onClick={
+                    index == 1
+                      ? () => viewProjects(item)
+                      : index == 0
+                      ? () => previousPoftfolio()
+                      : () => nextPortfolio()
+                  }
+                  src={`${item?.src}-${index == 1 ? "main.jpg" : "side.svg"}`}
+                />
+                <div
+                  className={`projects-description mt-[70px] mx-auto w-[550px] text-center ${
+                    index == 1 ? " block " : "hidden"
+                  }`}
+                >
+                  <p className="text-center text-[24px] font-bold">
+                    {item?.title}
+                  </p>
+                  <p className=" mt-[18px] font-normal">{item?.description}</p>
+                </div>
               </div>
+              {index == 1 && (
+                <img
+                  src="./forward-arrow.svg"
+                  className="arrow ml-[25px] pointer"
+                  onClick={() =>
+                    setTimeout(() => {
+                      nextPortfolio();
+                    }, 500)
+                  }
+                />
+              )}
             </div>
           ))}
         </div>
-        <div className="flex mt-[120px] max-w-max m-auto gap-[60px]">
+        <div className="flex mt-[120px] max-w-max m-auto gap-[60px] pointer">
           <p>LinkedIn</p>
           <p>GitHub</p>
           <p>Mail</p>
